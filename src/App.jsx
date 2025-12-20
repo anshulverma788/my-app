0413c63d08f2563fb146dc26e5f4df436c00859a
Anshul Verma <vanshul788@gmail.com>
2025-12-20 14:48:49 +0530

    fix routing for GitHub Pages using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import Index from './pages/Index';
-import Package from './pages/All-Package'; 
-import Destination from './pages/Destination'; 
+import Package from './pages/All-Package';
+import Destination from './pages/Destination';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
@@ -15,6 +16,7 @@ import Testimonials from './pages/Testimonials';
 import FAQ from './pages/FAQ';
 import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
+
 import Shimla from './pages/Package/Shimla';
 import Dharamshala from './pages/Package/Dharamshala';
 import KasolManikaran from './pages/Package/KasolManikaran';
@@ -25,32 +27,36 @@ import RishikeshHaridwar from './pages/Package/RishikeshHaridwar';
 import AuliJoshimath from './pages/Package/AuliJoshimath';
 import ShimlaManali from './pages/Package/Shimla-manali';
 import KedarnathBadrinath from './pages/Package/KedarnathBadrinath';
+
 import Adventure from './pages/experiences/Adventure';
 import Luxury from './pages/experiences/Luxury';
 import Cultural from './pages/experiences/Cultural';
 import Wellness from './pages/experiences/Wellness';
+
 import Search from './pages/booking/Search';
-import BookingPage from './pages/booking/Customize'; 
+import BookingPage from './pages/booking/Customize';
 import Confirmation from './pages/booking/Confirmation';
+
 import NotFound from './pages/NotFound';
+
 const queryClient = new QueryClient();
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
 
-      <BrowserRouter>
+      {/* ✅ HashRouter is REQUIRED for GitHub Pages */}
+      <HashRouter>
         <Routes>
+
+          {/* HOME */}
           <Route path="/" element={<Index />} />
-          
-          {/* Purana Package Route */}
-          <Route path="/Package" element={<Package />} />
 
-          {/* 👇 YEH HAI MAIN CHANGE: Ab '/destinations' par aapka naya page khulega */}
+          {/* MAIN PAGES */}
+          <Route path="/package" element={<Package />} />
           <Route path="/destinations" element={<Destination />} />
-
           <Route path="/about" element={<About />} />
-          <Route path="/Destination" element={<Destination />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/gallery" element={<Gallery />} />
@@ -61,35 +67,35 @@ const App = () => (
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
 
-          {/* Destination Routes (Specific) */}
-          <Route path="/Package/Shimla-manali" element={<ShimlaManali />} />
-          <Route path="/Package/Shimla" element={<Shimla />} />
-          <Route path="/Package/Dharamshala" element={<Dharamshala />} />
-          <Route path="/Package/KasolManikaran" element={<KasolManikaran />} />
-          <Route path="/Package/Spiti8day" element={<Spiti8day />} />
-          <Route path="/Package/Nainital4Days" element={<Nainital4Days />} />
-          <Route path="/Package/Mussoorie" element={<Mussoorie />} />
-          <Route path="/Package/RishikeshHaridwar" element={<RishikeshHaridwar />} />
-          <Route path="/Package/AuliJoshimath" element={<AuliJoshimath />} />
-          <Route path="/Package/KedarnathBadrinath" element={<KedarnathBadrinath />} />
+          {/* PACKAGE ROUTES */}
+          <Route path="/package/shimla-manali" element={<ShimlaManali />} />
+          <Route path="/package/shimla" element={<Shimla />} />
+          <Route path="/package/dharamshala" element={<Dharamshala />} />
+          <Route path="/package/kasol-manikaran" element={<KasolManikaran />} />
+          <Route path="/package/spiti-8day" element={<Spiti8day />} />
+          <Route path="/package/nainital-4days" element={<Nainital4Days />} />
+          <Route path="/package/mussoorie" element={<Mussoorie />} />
+          <Route path="/package/rishikesh-haridwar" element={<RishikeshHaridwar />} />
+          <Route path="/package/auli-joshimath" element={<AuliJoshimath />} />
+          <Route path="/package/kedarnath-badrinath" element={<KedarnathBadrinath />} />
 
-          {/* Experience Routes */}
+          {/* EXPERIENCES */}
           <Route path="/experiences/adventure" element={<Adventure />} />
           <Route path="/experiences/luxury" element={<Luxury />} />
           <Route path="/experiences/cultural" element={<Cultural />} />
           <Route path="/experiences/wellness" element={<Wellness />} />
 
-          {/* Booking Routes */}
+          {/* BOOKING */}
           <Route path="/booking/search" element={<Search />} />
-          <Route path="/book/:id" element={<BookingPage />} />
           <Route path="/booking/customize" element={<BookingPage />} />
           <Route path="/booking/confirmation" element={<Confirmation />} />
 
-          {/* 404 fallback */}
+          {/* 404 */}
           <Route path="*" element={<NotFound />} />
+
         </Routes>
-      </BrowserRouter>
-      
+      </HashRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
