76b7775fb3732d3e2812941c76a25c18357c7ffb
Anshul Verma <vanshul788@gmail.com>
2025-12-20 15:03:02 +0530

    fix homepage url for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-app/',
-  plugins: [ 
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    // tailwindcss()
+  base: '/my-app/',   // 👈 EXACT repo name
+
+  plugins: [
+    react(),
   ],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
